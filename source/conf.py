828c52cb5d8a42773590feefac2c0514b501c152
Antoine.Meheut <34522927+AntoineMeheut@users.noreply.github.com>
2025-07-10 15:05:38 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -84,8 +84,10 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #---sphinx-themes-----
-html_theme = 'rtcat_sphinx_theme'
-html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
+# html_theme = 'rtcat_sphinx_theme'
+# html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
